3f9c83b84f9b90a840f7b0ce1fd26e7559d944ba
zaccowan <zcowan@bellarmine.edu>
2024-03-26 23:50:31 -0400

    setup for lexer, parser, parsetree, and visitor

--- compiler/kcc.java
+++ compiler/kcc.java
@@ -1,12 +1,44 @@
 package compiler;
 
-public class KnightCompiler {
+import lexparse.KnightCodeLexer;
+import lexparse.KnightCodeParser;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class kcc {
 
     /*
     Needs to:
         Generate a parse tree for the Visitor
      */
 
-    AsmGen generator = new AsmGen()
+    public static void main(String[] args) throws IOException {
+
+
+        String file = args[0];
+        String programName = args[0].substring(file.indexOf('/')+1, file.indexOf('.'));
+        programName = Pattern.compile("^.").matcher(programName).replaceFirst(m -> m.group().toUpperCase());
+
+        System.out.println(programName);
+
+        AsmGen generator = new AsmGen(programName);
+        CharStream charStream = CharStreams.fromFileName(args[0]);
+        KnightCodeLexer lexer = new KnightCodeLexer(charStream);
+        TokenStream tokenStream = new CommonTokenStream(lexer);
+        KnightCodeParser parser = new KnightCodeParser(tokenStream);
+
+        ParseTree tree = parser.file();
+        KnightVisitor visitor = new KnightVisitor(generator);
+        visitor.visit(tree);
+
+        generator.writeToFile();
+    }
 
 }

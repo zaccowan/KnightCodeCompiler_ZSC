32d0167d662d5353f1be01d4edeadd3d4e54f84b
zaccowan <zcowan@bellarmine.edu>
2024-03-26 22:32:22 -0400

    Added boilerplate ASM code and method to write ClassWriter bytes to file.

--- compiler/AsmGen.java
+++ compiler/AsmGen.java
@@ -1,12 +1,47 @@
 package compiler;
 
+import org.objectweb.asm.*;
+import org.objectweb.asm.Opcodes;
+
 public class AsmGen {
 
-    public AsmGen() {
+    protected ClassWriter cw;
+    protected MethodVisitor mv;
+    private final String programName;
 
+    public AsmGen(String programName) {
+        this.programName = programName;
+
+        cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES); // New ClassWriter where stack map frames are automatically computed from scratch. (Per ASM Documentation)
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, programName, null, "java/lang/Object", null);
+
+        {   // Setup for Constructor.
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(Opcodes.ALOAD, 0);
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>","()V", false);
+
+            mv.visitInsn(Opcodes.RETURN); // Return from class
+            mv.visitMaxs(1,1); // max stack size of 1 and max number of local variable of 1
+            mv.visitEnd(); // visit end of class
+        }
+
+        mv = cw.visitMethod(Opcodes.ACC_PUBLIC+Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        mv.visitCode();
     }
 
     public void writeToFile() {
+        mv.visitInsn(Opcodes.RETURN); // Return from main method.
+        mv.visitMaxs(0,0); // maximum stack size and max number of local variable for main.
+        mv.visitEnd(); // end the main method.
 
+        cw.visitEnd(); // Termination point for class writer.
+
+        byte[] b = cw.toByteArray(); // Store data created by Class Writer to a byte array
+
+        Utilities.writeFile(b, "./output/" + programName + ".class"); // Write the byte array out to a class file
+
+        System.out.println("Done!"); // Print Completion Message
     }
+
 }

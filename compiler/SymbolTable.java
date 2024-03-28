183cc913be5c6e2323ddd78581c47748c977d687
zaccowan <zcowan@bellarmine.edu>
2024-03-27 20:23:02 -0400

    Basic symbol table with Variable value

--- compiler/SymbolTable.java
+++ compiler/SymbolTable.java
@@ -3,9 +3,19 @@ package compiler;
 import java.util.HashMap;
 
 public class SymbolTable {
-    protected HashMap<String, Variable> symbolTable;
+    private HashMap<String, Variable> symbolTable;
 
     SymbolTable() {
         symbolTable = new HashMap<>();
     }
+
+    public void setSymbol(String symbol) {
+        symbolTable.put(symbol, new Variable());
+    }
+
+    public void addEntry(String symbol, Object value, int VAR_TYPE ) {
+        symbolTable.put(symbol, new Variable(value, VAR_TYPE));
+    }
+
+
 }

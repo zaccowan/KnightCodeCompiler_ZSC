a31270a74a712dcaaa8897b2780904bce72599d7
zaccowan <zcowan@bellarmine.edu>
2024-03-29 00:20:06 -0400

    Variable now only stores variable location in var stack and variable type

--- compiler/Variable.java
+++ compiler/Variable.java
@@ -7,50 +7,23 @@ public class Variable {
     protected static final int TYPE_STRING = 2;
 
     private int varType;
-    private Object value;
+    private final int varIndex;
 
-
-
-    private final int stackIndex;
-
-    public Variable(int VAR_TYPE, int stackIndex) {
+    public Variable(int VAR_TYPE, int varIndex) {
         this.varType = VAR_TYPE;
-        this.stackIndex = stackIndex;
+        this.varIndex = varIndex;
     }
 
-    public Variable(String val, int VAR_TYPE, int stackIndex) {
-        this.value = new Object();
+    public Variable(String val, int VAR_TYPE, int varIndex) {
         this.varType = VAR_TYPE;
-        this.stackIndex = stackIndex;
-        switch(VAR_TYPE) {
-            case TYPE_INTEGER:
-                this.value = Integer.valueOf(val);
-                break;
-            case TYPE_STRING:
-                this.value = val;
-                break;
-        }
-    }
-
-    public void setValue(String val) {
-        switch(varType) {
-            case TYPE_INTEGER:
-                this.value = Integer.valueOf(val);
-                break;
-            case TYPE_STRING:
-                this.value = val;
-                break;
-        }
+        this.varIndex = varIndex;
     }
 
     public int getVarType() {
         return varType;
     }
 
-    public Object getValue() {
-        return value;
-    }
-    public int getStackIndex() {
-        return stackIndex;
+    public int getVarIndex() {
+        return varIndex;
     }
 }
